419a9e176b78af5f0b3c4911df73c2cd32c3a455
Chris Hall <chall@pangeare.com>
2014-09-04 19:55:35 -0500

    Populated the sublist from the settings array

--- client/views/sublist.js
+++ client/views/sublist.js
@@ -1,5 +1,10 @@
 enyo.kind({
   name: 'XV.SettingsSublist',
   kind: 'XV.Sublist',
-  items: []
+  create: enyo.inherit(function(sup) {
+    return function() {
+      this.items = xCore.getSettings();
+      sup.apply(this, arguments);
+    };
+  })
 });
